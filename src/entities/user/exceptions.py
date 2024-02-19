cf823e1d90561c22449f68e8e4604f7473f07351
Zlyden-1 <Zlyden-1@yandex.ru>
2024-02-19 16:48:52 +0400

    refactor(graph): :fire: removed unnessesary exception

--- src/entities/user/exceptions.py
+++ src/entities/user/exceptions.py
@@ -1,10 +1,9 @@
 from src.entities.base import PermitDeniedException
 
+
 class UserNotFoundException(PermitDeniedException):
     pass
 
-class ObjectNotFoundException(PermitDeniedException):
-    pass
 
-class ObjectTypeError(TypeError):
+class ObjectNotFoundException(PermitDeniedException):
     pass
