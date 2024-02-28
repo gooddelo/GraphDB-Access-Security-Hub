9772836392f396b5da4e6aca83a31f276224d7e8
Zlyden-1 <Zlyden-1@yandex.ru>
2024-02-28 13:59:32 +0400

    feat(api): :sparkles: added ud api for user

--- src/api/amqp/v1/users.py
+++ src/api/amqp/v1/users.py
@@ -2,7 +2,7 @@ from faststream.rabbit import RabbitRouter
 
 from src.config.amqp import GASH_EXCHANGE
 from src.api.amqp.v1.queues import UserQueuesV1
-from src.entities.user.dto import UserCreateDTO
+from src.entities.user.dto import UserCreateDTO, UserUpdateDTO, UserPropertiesDTO
 from src.entities.user.services import UserService
 
 
@@ -12,3 +12,13 @@ users_v1 = RabbitRouter()
 @users_v1.subscriber(queue=UserQueuesV1.CREATE, exchange=GASH_EXCHANGE)
 async def create_user(data: UserCreateDTO):
     await UserService.create(data)
+
+
+@users_v1.subscriber(queue=UserQueuesV1.UPDATE, exchange=GASH_EXCHANGE)
+async def update_user(data: UserUpdateDTO):
+    await UserService.update(data)
+
+
+@users_v1.subscriber(queue=UserQueuesV1.DELETE, exchange=GASH_EXCHANGE)
+async def delete_user(data: UserPropertiesDTO):
+    await UserService.delete(data)
--- src/tests/api/test_users.py
+++ src/tests/api/test_users.py
@@ -3,7 +3,7 @@ import uuid
 import pytest
 from faststream.rabbit import TestRabbitBroker
 
-from src.entities.user.dto import UserCreateDTO
+from src.entities.user.dto import UserCreateDTO, UserUpdateDTO
 from src.entities.scope.dto import ScopePropertiesDTO
 from src.entities.resource.dto import ResourcePropertiesDTO
 from src.entities.scope.exceptions import ScopeNotFoundException
@@ -90,3 +90,47 @@ class TestUserAPI:
                         queue=UserQueuesV1.CREATE,
                         exchange=GASH_EXCHANGE,
                     )
+
+    @pytest.mark.parametrize(
+        "user_nodes,scope_nodes",
+        (
+            ([uuid.uuid4()], []),
+            ([uuid.uuid4()], [uuid.uuid4(), uuid.uuid4(), uuid.uuid4()]),
+        ),
+        indirect=True,
+    )
+    async def test_update_user_scopes(self, user_nodes, scope_nodes):
+        user = user_nodes[0]
+        old_belong_scopes = scope_nodes[: len(scope_nodes) // 2]
+        new_belong_scopes = scope_nodes[len(scope_nodes) // 2 :]
+        for scope in old_belong_scopes:
+            await user.belong_scopes.connect(scope)
+            await user.own_scopes.connect(scope)
+        new_data = UserUpdateDTO[ScopePropertiesDTO, ResourcePropertiesDTO](
+            id_=user.id_,
+            old_role=user.role,
+            new_belong_scopes=[
+                ScopePropertiesDTO.model_validate(scope) for scope in new_belong_scopes
+            ],
+        )
+        async with TestRabbitBroker(broker) as test_brocker:
+            result = await test_brocker.publish(
+                message=new_data,
+                queue=UserQueuesV1.UPDATE,
+                exchange=GASH_EXCHANGE,
+            )
+        assert result is None
+        connected_belong_scopes = await user.belong_scopes.find_connected_nodes()
+        assert set(connected_belong_scopes) == set(new_belong_scopes)
+
+    @pytest.mark.parametrize("user_nodes", ([uuid.uuid4()],), indirect=True)
+    async def test_delete_user(self, user_nodes):
+        user = {"id_": user_nodes[0].id_, "role": user_nodes[0].attr}
+        async with TestRabbitBroker(broker) as test_brocker:
+            result = await test_brocker.publish(
+                message=user,
+                queue=UserQueuesV1.DELETE,
+                exchange=GASH_EXCHANGE,
+            )
+        assert result is None
+        assert await User.count() == 0
