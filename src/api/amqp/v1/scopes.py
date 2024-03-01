35410227a732897ba2c9b6bd057619b21595d6ce
Zlyden-1 <Zlyden-1@yandex.ru>
2024-03-01 15:22:36 +0400

    fix(api): :ambulance: fixed api dto propertien inconsistency
    got rid of "attr" keys in api dtos

--- src/api/amqp/v1/scopes.py
+++ src/api/amqp/v1/scopes.py
@@ -4,19 +4,23 @@ from src.config.amqp import GASH_EXCHANGE
 from src.api.amqp.v1.queues import ScopeQueuesV1
 from src.entities.scope.dto import ScopeCreateDTO, ScopeUpdateDTO, ScopePropertiesDTO
 from src.entities.scope.services import ScopeService
+from src.entities.user.dto import UserPropertiesDTO
+from src.entities.resource.dto import ResourcePropertiesDTO
 
 
 scopes_v1 = RabbitRouter()
 
 
 @scopes_v1.subscriber(queue=ScopeQueuesV1.CREATE, exchange=GASH_EXCHANGE)
-async def create_scope(data: ScopeCreateDTO):
+async def create_scope(data: ScopeCreateDTO[UserPropertiesDTO, ResourcePropertiesDTO]):
     await ScopeService.create(data)
 
+
 @scopes_v1.subscriber(queue=ScopeQueuesV1.UPDATE, exchange=GASH_EXCHANGE)
-async def update_scope(data: ScopeUpdateDTO):
+async def update_scope(data: ScopeUpdateDTO[UserPropertiesDTO, ResourcePropertiesDTO]):
     await ScopeService.update(data)
-            
+
+
 @scopes_v1.subscriber(queue=ScopeQueuesV1.DELETE, exchange=GASH_EXCHANGE)
 async def delete_scope(data: ScopePropertiesDTO):
     await ScopeService.delete(data)
--- src/api/amqp/v1/users.py
+++ src/api/amqp/v1/users.py
@@ -2,20 +2,22 @@ from faststream.rabbit import RabbitRouter
 
 from src.config.amqp import GASH_EXCHANGE
 from src.api.amqp.v1.queues import UserQueuesV1
-from src.entities.user.dto import UserCreateDTO, UserUpdateDTO, UserPropertiesDTO
 from src.entities.user.services import UserService
+from src.entities.scope.dto import ScopePropertiesDTO
+from src.entities.resource.dto import ResourcePropertiesDTO
+from src.entities.user.dto import UserCreateDTO, UserUpdateDTO, UserPropertiesDTO
 
 
 users_v1 = RabbitRouter()
 
 
 @users_v1.subscriber(queue=UserQueuesV1.CREATE, exchange=GASH_EXCHANGE)
-async def create_user(data: UserCreateDTO):
+async def create_user(data: UserCreateDTO[ScopePropertiesDTO, ResourcePropertiesDTO]):
     await UserService.create(data)
 
 
 @users_v1.subscriber(queue=UserQueuesV1.UPDATE, exchange=GASH_EXCHANGE)
-async def update_user(data: UserUpdateDTO):
+async def update_user(data: UserUpdateDTO[ScopePropertiesDTO, ResourcePropertiesDTO]):
     await UserService.update(data)
 
 
